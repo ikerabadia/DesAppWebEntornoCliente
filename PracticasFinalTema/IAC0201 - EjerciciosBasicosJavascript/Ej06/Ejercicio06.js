1c680404d3f4fc095b8e07cd53d1ec097d97f107
Iker Abadia Conejos <ikerabadia999@gmail.com>
2021-09-16 17:20:55 +0200

    Ejercicio 6 completo

---
+++ PracticasFinalTema/IAC0201 - EjerciciosBasicosJavascript/Ej06/Ejercicio06.js
@@ -0,0 +1,39 @@
+//4- Recoge el alto y el ancho de una tabla mediante prompt. Escribe en el body (document.write()) 
+//una tabla de esas medidas, que muestre los números desde el 1 hasta el número de celdas.
+//Por ejemplo, para las entradas: alto 2, ancho 5.
+//El programa debe escribir en la página:
+//1	  2	  3	  4	  5
+//6	  7	  8	  9	  10
+
+//Ayuda: document.write("<table>");
+
+var alto = prompt("Introduce el ALTO de la tabla");
+var ancho = prompt("Introduce el ANCHO de la tabla");
+var colspan = prompt("Introduce el COLSPAN para las columnas");
+var contador = 1;
+
+document.write("<table border>");
+for (let i = 0; i < alto; i++) { //Generacion de la altura de la tabla
+    document.write("<tr>");
+
+    for (let j = 0; j < ancho; j++) {
+
+        if (i > 0) {
+            if (ancho - j < colspan) {
+                document.write("<td>" + contador + "</td>");
+            } else {
+                document.write("<td colspan = " + colspan + ">" + contador + "</td>");
+                j = j + parseInt(colspan) - 1;
+            }
+        } else {
+            document.write("<td>" + contador + "</td>");
+        }
+
+        contador++;
+    }
+
+    document.write("</tr>");
+}
+document.write("</table>");
+
+
