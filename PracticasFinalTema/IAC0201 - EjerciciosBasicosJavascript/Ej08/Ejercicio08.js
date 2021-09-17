f2d46aa39bcf3adc0bd9858983de3f9696f53690
Iker Abadia <ikerabadia999@gmail.com>
2021-09-17 03:28:22 +0200

    Ejercicio 8 Optimizado con funciones

--- PracticasFinalTema/IAC0201 - EjerciciosBasicosJavascript/Ej08/Ejercicio08.js
+++ PracticasFinalTema/IAC0201 - EjerciciosBasicosJavascript/Ej08/Ejercicio08.js
@@ -17,49 +17,19 @@ function GenerarCampos() {
             generarCampoTexto();
             break;
         case 2: //password
-            var nombreCampo = prompt("Introduzca el nombre del campo");
-            var valorPorDefecto = prompt("Introduzca el valor que tendra por defecto el campo");
-            document.write("<input type=\"password\" name = \"" + nombreCampo + "\" placeholder=\"" + valorPorDefecto + "\"><br>");
+            generarCampoPassword();
             break;
         case 3: //boton
-            var nombreCampo = prompt("Introduzca el nombre del boton");
-            var valorPorDefecto = prompt("Introduzca el valor del boton");
-            document.write("<button>" + valorPorDefecto + "</button><br>");
+            generarCampoBoton();
             break;
         case 4: //select
-            var pregunta = prompt("Introduzca la pregunta que se realizara en el select");
-            document.write("<label for=\"select\">" + pregunta + "</label>");
-            document.write("<select name=\"select\" id=\"select\">");
-            var numeroOpciones = prompt("Escriba el numero de opciones del select");
-            for (let i = 0; i < numeroOpciones; i++) {    
-                var textoOpcion = prompt("Introduzca el nombre de la opcion");            
-                document.write("<option value=\"" + textoOpcion + "\">" + textoOpcion + "</option>");
-            }
-            document.write("</select>");            
+            generarCampoSelect();
             break;
         case 5: //radio
-            var pregunta = prompt("Introduzca la pregunta que se realizara en el radio button");
-            document.write("<p>" + pregunta + "</p>");
-            document.write("<form>");
-            var numeroOpciones = prompt("Escriba el numero de opciones del radio button");
-            for (let i = 0; i < numeroOpciones; i++) {    
-                var textoOpcion = prompt("Introduzca el nombre de la opcion");            
-                document.write("<input type=\"radio\" id=\""+textoOpcion+"\" name=\""+textoOpcion+"\" value=\""+textoOpcion+"\">");
-                document.write("<label for=\""+textoOpcion+"\">" + textoOpcion + "</label><br>");
-            }
-            document.write("</form>");
+            generarCampoRadioBtn();
             break;
         case 6: //checkbox
-            var pregunta = prompt("Introduzca la pregunta que se realizara en el check box");
-            document.write("<p>" + pregunta + "</p>");
-            document.write("<form>");
-            var numeroOpciones = prompt("Escriba el numero de opciones del radio button");
-            for (let i = 0; i < numeroOpciones; i++) {    
-                var textoOpcion = prompt("Introduzca el nombre de la opcion");            
-                document.write("<input type=\"checkbox\" id=\""+textoOpcion+"\" name=\""+textoOpcion+"\" value=\""+textoOpcion+"\">");
-                document.write("<label for=\""+textoOpcion+"\">" + textoOpcion + "</label><br>");
-            }
-            document.write("</form>");
+            generarCampoCheckbox();
             break;        
     }
 }
@@ -68,4 +38,54 @@ function generarCampoTexto() {
     var nombreCampo = prompt("Introduzca el nombre del campo");
     var valorPorDefecto = prompt("Introduzca el valor que tendra por defecto el campo");
     document.write("<input type=\"text\" name = \"" + nombreCampo + "\" placeholder=\"" + valorPorDefecto + "\"><br>");
+}
+
+function generarCampoPassword() {
+    var nombreCampo = prompt("Introduzca el nombre del campo");
+    var valorPorDefecto = prompt("Introduzca el valor que tendra por defecto el campo");
+    document.write("<input type=\"password\" name = \"" + nombreCampo + "\" placeholder=\"" + valorPorDefecto + "\"><br>");
+}
+
+function generarCampoBoton() {
+    var nombreCampo = prompt("Introduzca el nombre del boton");
+    var valorPorDefecto = prompt("Introduzca el valor del boton");
+    document.write("<button>" + valorPorDefecto + "</button><br>");
+}
+
+function generarCampoSelect() {
+    var pregunta = prompt("Introduzca la pregunta que se realizara en el select");
+    document.write("<label for=\"select\">" + pregunta + "</label>");
+    document.write("<select name=\"select\" id=\"select\">");
+    var numeroOpciones = prompt("Escriba el numero de opciones del select");
+    for (let i = 0; i < numeroOpciones; i++) {    
+        var textoOpcion = prompt("Introduzca el nombre de la opcion");            
+        document.write("<option value=\"" + textoOpcion + "\">" + textoOpcion + "</option>");
+    }
+    document.write("</select>");   
+}
+
+function generarCampoRadioBtn() {
+    var pregunta = prompt("Introduzca la pregunta que se realizara en el radio button");
+    document.write("<p>" + pregunta + "</p>");
+    document.write("<form>");
+    var numeroOpciones = prompt("Escriba el numero de opciones del radio button");
+    for (let i = 0; i < numeroOpciones; i++) {    
+        var textoOpcion = prompt("Introduzca el nombre de la opcion");            
+        document.write("<input type=\"radio\" id=\""+textoOpcion+"\" name=\""+textoOpcion+"\" value=\""+textoOpcion+"\">");
+        document.write("<label for=\""+textoOpcion+"\">" + textoOpcion + "</label><br>");
+    }
+    document.write("</form>");
+}
+
+function generarCampoCheckbox() {
+    var pregunta = prompt("Introduzca la pregunta que se realizara en el check box");
+    document.write("<p>" + pregunta + "</p>");
+    document.write("<form>");
+    var numeroOpciones = prompt("Escriba el numero de opciones del radio button");
+    for (let i = 0; i < numeroOpciones; i++) {    
+        var textoOpcion = prompt("Introduzca el nombre de la opcion");            
+        document.write("<input type=\"checkbox\" id=\""+textoOpcion+"\" name=\""+textoOpcion+"\" value=\""+textoOpcion+"\">");
+        document.write("<label for=\""+textoOpcion+"\">" + textoOpcion + "</label><br>");
+    }
+    document.write("</form>");
 }
