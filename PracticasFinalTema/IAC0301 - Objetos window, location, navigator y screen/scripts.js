d91031ccc699ab41ffecbad4d91fe35666fc8b0c
Iker Abadia <ikerabadia999@gmail.com>
2021-10-01 13:20:23 +0200

    IAC0301 - Completada

--- PracticasFinalTema/IAC0301 - Objetos window, location, navigator y screen/scripts.js
+++ PracticasFinalTema/IAC0301 - Objetos window, location, navigator y screen/scripts.js
@@ -53,3 +53,42 @@ function getHostname() {
 function pagesOnHistory() {
     document.getElementById("numberPages").innerHTML = history.length;
 }
+
+//Datos del navegador
+function navigatorData() {
+    document.getElementById("navigatorData").innerHTML = 
+    "navigator.appCodeName is " + navigator.appCodeName;
+    document.getElementById("navigatorData2").innerHTML =
+    "navigator.product is " + navigator.product;
+    document.getElementById("navigatorData3").innerHTML =
+    "navigator.language is " + navigator.language;
+}
+
+//Sistema opertativo
+function sistemaOperativo() {
+    document.getElementById("sistemaOperativo").innerHTML = 
+    "navigator.platform is " + navigator.platform;
+}
+
+//Cronometro
+var c = 0;
+function myCounter() {
+  document.getElementById("demo").innerHTML = ++c;
+}
+
+//Reloj
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('reloj').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
