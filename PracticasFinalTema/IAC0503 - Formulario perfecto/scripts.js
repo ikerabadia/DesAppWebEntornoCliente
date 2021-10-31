05b16c62ad33dcffea33ab4fc0cec6d87e50003d
Iker Abadia <ikerabadia999@gmail.com>
2021-10-31 21:41:36 +0100

    html y css completados, intentando empezar el añadir extudios

---
+++ PracticasFinalTema/IAC0503 - Formulario perfecto/scripts.js
@@ -0,0 +1,12 @@
+var estudios = 1;
+var experiencia = 1;
+
+function añadirEstudios() {
+    var contenedorEstudios = document.getElementById("divContenedorEstudios");
+    var div = document.createElement('div');
+    div.innerHTML = "";
+    document.createEle
+}
+function añadirExperiencia() {
+    
+}
