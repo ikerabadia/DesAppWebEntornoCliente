8a6306cf6691522f351695d3352700463625ff65
Iker Abadia <ikerabadia999@gmail.com>
2021-11-01 02:28:47 +0100

    IAC0503 - Formulario perfecto - Añadir y eliminar Experiencias y estudios completado

--- PracticasFinalTema/IAC0503 - Formulario perfecto/scripts.js
+++ PracticasFinalTema/IAC0503 - Formulario perfecto/scripts.js
@@ -4,9 +4,21 @@ var experiencia = 1;
 function añadirEstudios() {
     var contenedorEstudios = document.getElementById("divContenedorEstudios");
     var div = document.createElement('div');
-    div.innerHTML = "";
-    document.createEle
+    estudios++;
+    div.innerHTML = "<input type=\"text\" name=\"tituloEstudios"+estudios+"\" class=\"tituloEstudios\" placeholder=\"titulo\"><input type=\"date\" name=\"fechaFinalizacionEstudios"+estudios+"\" class=\"fechaFinalizacionEstudios\" placeholder=\"Fecha Finalizacion\"><input type=\"text\" name=\"centroEstudios"+estudios+"\" class=\"centroEstudios\" placeholder=\"Centro\"><a class=\"btnEliminarElemento\" onclick=\"eliminarElemento(this)\"><i class=\"fa fa-minus-square fa-2x\" aria-hidden=\"true\"></i></a>";
+    div.className = "divEstudio"
+    contenedorEstudios.appendChild(div);
 }
+
 function añadirExperiencia() {
-    
+    var contenedorExperienciaLaboral = document.getElementById("divContenedorExperienciaLaboral");
+    var div = document.createElement('div');
+    experiencia++;
+    div.innerHTML = "<div class=\"cabeceraExperienciaLaboral\"><input type=\"text\" name=\"inputEmpresa"+experiencia+"\" placeholder=\"Empresa\" class=\"inputEmpresa\"><input type=\"text\" name=\"inputPuesto"+experiencia+"\" placeholder=\"Puesto\" class=\"inputPuesto\"><input type=\"date\" name=\"fechaFinalizacionExperienciaLaboral"+experiencia+"\" placeholder=\"Fecha finalizacion\" class=\"fechaFinalizacionExperienciaLaboral\"></div><div  class=\"divDescripcionExperienciaLaboral\"><textarea name=\"descripcionExperienciaLaboral"+experiencia+"\" class=\"descripcionExperienciaLaboral\" placeholder=\"Descripcion\"></textarea></div><a class=\"btnEliminarElemento\" onclick=\"eliminarElemento(this)\"><i class=\"fa fa-minus-square fa-2x\" aria-hidden=\"true\"></i></a>";
+    div.className = "divExperienciaLaboral";
+contenedorExperienciaLaboral.appendChild(div);
+}
+
+function eliminarElemento(elemento) {
+    elemento.parentNode.parentNode.removeChild(elemento.parentNode);
 }
