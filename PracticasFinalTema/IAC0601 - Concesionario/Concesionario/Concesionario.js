1a37e7667eeac032ca22718926a4ea3e2ca08d52
Iker Abadia Conejos <ikerabadia999@gmail.com>
2021-11-05 20:05:23 +0100

    concesionario ahora esta hecho tipo java

--- PracticasFinalTema/IAC0601 - Concesionario/Concesionario/Concesionario.js
+++ PracticasFinalTema/IAC0601 - Concesionario/Concesionario/Concesionario.js
@@ -1,34 +1,36 @@
+class Concesionario {
 
-Concesionario = function() {
-    this.clientes = [];
-    this.coches = [];
+    constructor() {
+        this.clientes = [];
+        this.coches = [];
+    }
+
+    abrir() {
+        var coche1 = new Coche("2007FJH", "BMW", "X5", 214);
+        this.anadirCoche(coche1);
+        var coche2 = new Coche("2314GBH", "Audi", "TT", 250);
+        this.anadirCoche(coche2);
+        var coche3 = new Coche("8475HAT", "Chevrolet", "Camaro", 405);
+        this.anadirCoche(coche3);
+    }
+
+    anadirCoche(coche){
+        this.coches.push(coche);
+    }
 }
 
-Concesionario.prototype.abrir = function(){
-    coche1 = new Coche("2007FJH","BMW","X5",214);
-    this.anadirCoche(coche1);
-    coche2 = new Coche("2314GBH","Audi","TT",250);
-    this.anadirCoche(coche2);
-    coche3 = new Coche("8475HAT","Chevrolet","Camaro",405);
-    this.anadirCoche(coche3);
-}
-
-Concesionario.prototype.nuevoCliente = function(cliente) {
+Concesionario.prototype.nuevoCliente = function (cliente) {
     this.clientes.push(cliente);
 }
 
-Concesionario.prototype.anadirCoche = function(coche){
-    this.coches.push(coche);
-}
-
-Concesionario.prototype.venta = function(matriculaCoche, dniCliente) {
+Concesionario.prototype.venta = function (matriculaCoche, dniCliente) {
     var cliente = getCliente(dniCliente);
     var coche = getCoche(matriculaCoche);
 
     cliente.comprarCoche(coche);
 }
 
-Concesionario.prototype.getCliente = function(dniCliente){
+Concesionario.prototype.getCliente = function (dniCliente) {
     for (var i = 0; i < this.clients.length; i++) {
         var client = this.clients[i];
         if (client.dni == dniCliente) {
@@ -38,11 +40,11 @@ Concesionario.prototype.getCliente = function(dniCliente){
     return false;
 }
 
-Concesionario.prototype.getCoche = function(matriculaCoche) {
+Concesionario.prototype.getCoche = function (matriculaCoche) {
     for (var i = 0; i < this.coches.length; i++) {
         var coche = this.coches[i];
         if (coche.matricula == matriculaCoche) {
-            this.coches.splice(i,1);
+            this.coches.splice(i, 1);
             return coche;
         }
     }
